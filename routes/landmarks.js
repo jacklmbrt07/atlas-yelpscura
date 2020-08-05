cc97f26c4a50f4ce3159a6389a41106539b36c74
jacklmbrt07 <jacklmbrt@gmail.com>
2020-08-05 15:02:08 -0700

    added edit button

--- routes/landmarks.js
+++ routes/landmarks.js
@@ -4,6 +4,7 @@ const landmarksCtrl = require("../controllers/landmarks");
 
 router.post("/cities/:id/landmarks", landmarksCtrl.create);
 router.delete("/cities/:city_id/landmarks/:landmark_id", landmarksCtrl.delete);
-// router.put("/cities/:id/landmarks/:id", landmarksCtrl.update)
+// router.put("/landmarks/:id/edit", landmarksCtrl.edit)
+// router.put("/landmarks/:id", landmarksCtrl.update)
 
 module.exports = router;
