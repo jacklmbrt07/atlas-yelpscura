20b80e8626ad324c09572c3be78053d54a106ed2
jacklmbrt07 <jacklmbrt@gmail.com>
2020-08-06 10:34:23 -0700

    moved cities router

--- routes/cities.js
+++ routes/cities.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const citiesCtrl = require("../controllers/cities");
 
-router.get("/new", citiesCtrl.new);
 router.post("/", citiesCtrl.create);
 router.get("/", citiesCtrl.index);
+router.get("/new", citiesCtrl.new);
 router.get("/:id", citiesCtrl.show);
 
 module.exports = router;
